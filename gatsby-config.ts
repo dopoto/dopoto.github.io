a848175d27c0e0d7b480759a2c2a68f31fa9bebc
dopoto <dopoto@gmail.com>
2024-09-22 07:32:47 +0300

    origin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -141,7 +141,7 @@ module.exports = {
             // Avoids sending pageview hits from custom paths
             exclude: ["/preview/**", "/do-not-track/me/too/"],
             // Defaults to https://www.googletagmanager.com
-            origin: "dopoto.github.io",
+            //origin: "",
             // Delays processing pageview events on route update (in milliseconds)
             delayOnRouteUpdate: 0,
           },
