daadbff6b759d46ffad769c043a9463c97e0a079
dopoto <dopoto@gmail.com>
2025-02-17 13:25:39 +0200

    post dynamic breadcrumb

--- components/DemoLink.tsx
+++ components/DemoLink.tsx
@@ -10,12 +10,14 @@ interface Props {
 // </div>
 export default function DemoLink({ url }: Props) {
   return (
-    <div className="flex flex-row gap-3 rounded-md bg-gray-100 p-3 align-middle">
+    <div className="flex flex-row gap-3 rounded-md bg-gray-100 p-3 align-middle dark:bg-gray-900">
       <div className="flex items-center">
         <SocialIcon href={url} kind="demo" />
       </div>
       <div>
-        <div className="text-sm font-semibold text-gray-600 antialiased">DEMO</div>
+        <div className="text-sm font-semibold text-gray-600 antialiased dark:text-gray-300">
+          DEMO
+        </div>
         <Link target="_blank" href={url}>
           {url}
         </Link>
--- components/GitHubRepoLink.tsx
+++ components/GitHubRepoLink.tsx
@@ -7,12 +7,14 @@ interface Props {
 
 export default function GitHubRepoLink({ url }: Props) {
   return (
-    <div className="flex flex-row gap-3 rounded-md bg-gray-100 p-3 align-middle">
+    <div className="flex flex-row gap-3 rounded-md bg-gray-100 p-3 align-middle dark:bg-gray-900">
       <div className="flex items-center">
         <SocialIcon href={url} kind="github" />
       </div>
       <div>
-        <div className="text-sm font-semibold text-gray-600 antialiased">SOURCE CODE</div>
+        <div className="text-sm font-semibold text-gray-600 antialiased dark:text-gray-300">
+          SOURCE CODE
+        </div>
         <Link target="_blank" href={url}>
           {url}
         </Link>
