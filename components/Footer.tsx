6623a0a79ce4e233532da401431e96c90b373114
dopoto <dopoto@gmail.com>
2025-02-19 08:58:42 +0200

    Post - serializing big strings

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,8 @@ export default function Footer() {
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
-          </Link>
+          </Link>{' '}
+          | <Link href="https://github.com/dopoto/dopoto.github.io">Blog source code</Link>
         </div>
       </div>
     </footer>
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,18 +11,10 @@ const projectsData: Project[] = [
   {
     name: 'RemoteBeep',
     description: 'Play sounds remotely on multiple devices',
-    tech: ['.NET 8', 'SignalR', 'Angular', 'NGRX'],
+    tech: ['.NET 8', 'SignalR', 'Angular', 'NGRX', 'TypeScript'],
     repoUrl: 'https://github.com/dopoto/remote-beep-frontend',
     repoText: '/dopoto/remote-beep-frontend',
     demoUrl: 'https://remote-beep-frontend.vercel.app',
-  },
-  {
-    name: 'Dynamic navbar',
-    description: 'A Next.js dynamic breadcrumb component',
-    tech: ['Next.js'],
-    repoUrl: 'https://github.com/dopoto/next-dynamic-breadcrumbs',
-    repoText: '/dopoto/next-dynamic-breadcrumbs',
-    demoUrl: 'https://next-dynamic-breadcrumbs.vercel.app/manager',
   },
   {
     name: 'TEPS',
@@ -31,6 +23,14 @@ const projectsData: Project[] = [
     repoUrl: 'https://github.com/dopoto/teps',
     repoText: '/dopoto/teps',
     demoUrl: 'https://teps.vercel.app',
+  },
+  {
+    name: 'Dynamic navbar',
+    description: 'A Next.js dynamic breadcrumb component',
+    tech: ['Next.js', 'TypeScript', 'React'],
+    repoUrl: 'https://github.com/dopoto/next-dynamic-breadcrumbs',
+    repoText: '/dopoto/next-dynamic-breadcrumbs',
+    demoUrl: 'https://next-dynamic-breadcrumbs.vercel.app/manager',
   },
 ]
 
