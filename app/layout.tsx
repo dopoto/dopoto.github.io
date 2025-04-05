d85003d6b8fc367032b02261b046c77f3d72c825
dopoto <dopoto@gmail.com>
2025-04-05 09:39:17 +0300

    Fix default title

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ const space_grotesk = Space_Grotesk({
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
   title: {
-    default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
+    default: `${siteMetadata.headerTitle} - ${siteMetadata.title}`,
+    template: `%s | ${siteMetadata.headerTitle} - ${siteMetadata.title}`,
   },
   description: siteMetadata.description,
   openGraph: {
